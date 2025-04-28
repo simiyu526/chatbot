57027c2d372df069f2b09484b62de660997b1aa0
Hopkin Simiyu <netflix2006opi@gmail.com>
2025-04-28 11:07:31 +0300

    Added clipboard

--- src/components/MessageBubble.tsx
+++ src/components/MessageBubble.tsx
@@ -1,5 +1,6 @@
 import ReactMarkdown from "react-markdown";
-
+import { FaClipboard } from "react-icons/fa";
+import { useState } from "react";
 type Message = {
   role: "user" | "ai" | "error";
   content: string;
@@ -12,17 +13,21 @@ interface MessageBubbleProps {
   onErrorClick?: (message: Message) => void;
 }
 
-export default function MessageBubble({ message, onErrorClick }: MessageBubbleProps) {
+export default function MessageBubble({
+  message,
+  onErrorClick,
+}: MessageBubbleProps) {
   const handleClick = () => {
     if (message.role === "error" && onErrorClick) {
       onErrorClick(message);
     }
   };
-
+const [visible,setVisible]=useState(false)
   const bubbleStyles = {
     user: "bg-slate-800 text-white rounded-br-none",
     ai: "text-zinc-900 break-words font-lg rounded-bl-none",
-    error: "bg-red-300 text-black rounded-bl-none cursor-pointer hover:bg-red-200",
+    error:
+      "bg-red-300 text-black rounded-bl-none cursor-pointer hover:bg-red-200",
   };
 
   const timeStyles = {
@@ -31,19 +36,40 @@ export default function MessageBubble({ message, onErrorClick }: MessageBubblePr
     error: "text-red-600",
   };
 
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text.replace(/^[🚫✅🤖] /, ""));
+    alert("Coppied")
+  };
+
   return (
-    <div className={`flex ${message.role === "user" ? "justify-end" : "justify-start"}`}>
+    <>
+    <div
+      className={`flex ${
+        message.role === "user" ? "justify-end" : "justify-start"
+      }`}
+    >
       <div
-        className={`max-w-3xl rounded-lg px-4 py-3 ${bubbleStyles[message.role]}`}
+        className={`max-w-3xl rounded-lg px-4 py-3 ${
+          bubbleStyles[message.role]
+        }`}
         onClick={handleClick}
       >
         <div className="whitespace-pre-wrap">
           <ReactMarkdown>{message.content}</ReactMarkdown>
         </div>
+        <div className="flex space-x-2">
         <div className={`text-xs mt-1 ${timeStyles[message.role]}`}>
-          {message.timestamp?.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" })}
+          {message.timestamp?.toLocaleTimeString([], {
+            hour: "2-digit",
+            minute: "2-digit",
+          })}
+        </div>
+        <button onClick={() => copyToClipboard(message.content)}>
+            <FaClipboard color="pink" />
+          </button>
         </div>
       </div>
     </div>
+    </>
   );
 }
